94b0b06b5476e64821514ed39f39358dd0feaa93
kalebyvaladares <kalebyvaladares04@gmail.com>
2024-05-17 16:39:33 -0300

    Criadas paginas criar e editar

--- WebCRUD/Controllers/ContatoController.cs
+++ WebCRUD/Controllers/ContatoController.cs
@@ -8,5 +8,17 @@ namespace WebCRUD.Controllers
         {
             return View();
         }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult Apagar()
+        {
+            return View();
+        }
     }
 }
