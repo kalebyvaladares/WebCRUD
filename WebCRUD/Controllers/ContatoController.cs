00cd0ec856d38b8bbb5f461e05e137cf353df169
kalebyvaladares <kalebyvaladares04@gmail.com>
2024-05-17 13:27:57 -0300

    modificações e aba contatos

---
+++ WebCRUD/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebCRUD.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
