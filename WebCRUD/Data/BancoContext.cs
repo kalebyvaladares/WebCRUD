1c7eb831a1a79431f2e471240ca28754f66e9e91
kalebyvaladares <kalebyvaladares04@gmail.com>
2024-05-19 14:12:19 -0300

    ajustes em editar e apagar, e model/context criados

---
+++ WebCRUD/Data/BancoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebCRUD.Controllers;
+using WebCRUD.Models;
+
+namespace WebCRUD.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options) { }
+
+        public DbSet<ContatoModel> Contatos { get; set; }
+    }
+
+}
